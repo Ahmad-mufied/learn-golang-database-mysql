9555a57f679758159fef1d97013a5b92bf49f18e
Ahmad-mufied <teranggelap01@gmail.com>
2023-02-08 17:37:09 +0700

    contoh dari penerapan query yang rentan terkena sql injection

--- sql_test.go
+++ sql_test.go
@@ -91,3 +91,34 @@ func TestQuerySqlComplex(t *testing.T) {
 		fmt.Println("Created At:", createdAt)
 	}
 }
+
+func TestSqlInjection(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin"
+	password := "admin"
+
+	script := "SELECT username FROM user WHERE username = '" + username +
+		"'AND password = '" + password + "' LIMIT 1"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Success login", username)
+	} else {
+		fmt.Println("Gagal Login")
+	}
+	
+}
